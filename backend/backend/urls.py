d7f21fe14abfeac85c4f0bc50e91f4ac989a6f2a
SoulflareRC <chenrc91@gmail.com>
2024-01-04 21:02:08 -0800

    integrated frontend in docker setup

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.http import HttpResponse
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("app/",include("app.urls")),
     path("auth/",include("auth.urls")),
+    path("healthcheck/",lambda x:HttpResponse())
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
